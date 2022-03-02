9e1513315d1a02437a015f4c8337b0d89d6da521
b-tanvir <b_tanvir@outlook.com>
2022-03-03 02:45:04 +0600

    implementing details section

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const loadSearchResult = () => {
           const div = document.createElement("div");
           div.classList.add = "col";
           div.innerHTML = `
-        <div class="card" id="card">
+        <div class="card" onclick="exploreDetails('${phone.slug}')">
               <img src="${phone.image}" class="card-img-top w-75 mx-auto mt-2" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">${phone.brand}</h5>
@@ -43,18 +43,57 @@ const loadSearchResult = () => {
                 </button>
               </div>
         `;
-          controlDisplayProperty("spinning", "none");
           displaySection.appendChild(div);
           count++;
         }
       });
-    } else {
-      controlDisplayProperty("not-found", "block");
     }
+    controlDisplayProperty("spinning", "none");
+    controlDisplayProperty("not-found", "block");
   }
 };
 
 /* implementing details view of a single phone*/
+const exploreDetails = (id) => {
+  const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+  fetch(url).then((res) =>
+    res.json().then((data) => {
+      if (data.status) {
+        console.log(data);
+        const detailsSection = document.getElementById("details");
+        detailsSection.textContent = "";
+        const div = document.createElement("div");
+        const phone = data.data;
+        const mainFeatures = phone.mainFeatures;
+        div.classList.add("card");
+        div.classList.add("mx-auto");
+        div.classList.add("w-75");
+
+        let releaseDate;
+        if (typeof phone.releaseDate == "string") {
+          releaseDate = phone.releaseDate;
+        } else {
+          releaseDate = "No release date found";
+        }
+        div.innerHTML = `
+    <img src="${phone.image}"  class="card-img-top mx-auto mt-2 w-25" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${phone.name}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Display: ${mainFeatures.displaySize}</li>
+            <li class="list-group-item">Release data: ${releaseDate}</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Chipset: ${mainFeatures.chipSet}</li>
+            <li class="list-group-item">Storage: ${mainFeatures.storage}</li>
+          </ul>
+        </div>
+    `;
+        detailsSection.appendChild(div);
+      }
+    })
+  );
+};
 
 /* utility functions */
 const controlDisplayProperty = (id, status) => {
