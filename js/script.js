06f869342ddc738b538101a3ecf7d50f1740f75a
b-tanvir <b_tanvir@outlook.com>
2022-03-03 03:31:42 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -59,26 +59,34 @@ const loadSearchResult = () => {
 /* implementing details view of a single phone*/
 const exploreDetails = (id) => {
   const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+
+  /* Fetching data through API*/
   fetch(url).then((res) =>
     res.json().then((data) => {
-      if (data.status) {
-        console.log(data);
-        const detailsSection = document.getElementById("details");
-        detailsSection.textContent = "";
-        const div = document.createElement("div");
-        const phone = data.data;
-        const mainFeatures = phone.mainFeatures;
-        div.classList.add("card");
-        div.classList.add("mx-auto");
-        div.classList.add("w-75");
+      displayDetails(data);
+    })
+  );
 
-        let releaseDate;
-        if (typeof phone.releaseDate == "string") {
-          releaseDate = phone.releaseDate;
-        } else {
-          releaseDate = "No release date found";
-        }
-        div.innerHTML = `
+  /* Displaying details data*/
+  function displayDetails(data) {
+    if (data.status) {
+      console.log(data);
+      const detailsSection = document.getElementById("details");
+      detailsSection.textContent = "";
+      const div = document.createElement("div");
+      const phone = data.data;
+      const mainFeatures = phone.mainFeatures;
+      div.classList.add("card");
+      div.classList.add("mx-auto");
+      div.classList.add("w-75");
+
+      let releaseDate;
+      if (typeof phone.releaseDate == "string") {
+        releaseDate = phone.releaseDate;
+      } else {
+        releaseDate = "No release date found";
+      }
+      div.innerHTML = `
     <img src="${phone.image}"  class="card-img-top mx-auto mt-2 w-25" alt="..." />
           <div class="card-body">
             <h5 class="card-title">${phone.name}</h5>
@@ -92,10 +100,9 @@ const exploreDetails = (id) => {
           </ul>
         </div>
     `;
-        detailsSection.appendChild(div);
-      }
-    })
-  );
+      detailsSection.appendChild(div);
+    }
+  }
 };
 
 /* utility functions */
